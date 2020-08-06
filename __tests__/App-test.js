d93f7b5a8b07cd562f098780feb1b3a64f18cbd2
Kubil Ismail <bilkisismail07@gmail.com>
2020-08-07 03:11:51 +0700

    fix root directory

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,10 +1,10 @@
-/**
+x/**
  * @format
  */
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
+ rootimport App from '../src/App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
---
+++ src/App.js
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import React, { Component } from 'react';
+import { SafeAreaView, Text } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <SafeAreaView>
+        <Text>Anjing loe</Text>
+      </SafeAreaView>
+    );
+  }
+}
