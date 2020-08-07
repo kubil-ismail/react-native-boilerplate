34980e6fa40f8fe3d1e3b3c15b065425203a774a
Kubil Ismail <bilkisismail07@gmail.com>
2020-08-07 07:43:33 +0700

    use splash screen

--- android/app/src/main/java/com/myproject/MainActivity.java
+++ android/app/src/main/java/com/myproject/MainActivity.java
@@ -1,5 +1,6 @@
 package com.myproject;
-
+import android.os.Bundle;
+import org.devio.rn.splashscreen.SplashScreen;
 import com.facebook.react.ReactActivity;
 
 public class MainActivity extends ReactActivity {
@@ -12,4 +13,10 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "myproject";
   }
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen.show(this);
+        super.onCreate(savedInstanceState);
+    }
 }
--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 /* eslint-disable prettier/prettier */
 import React, { Component } from 'react';
+// Splashscreen
+import SplashScreen from 'react-native-splash-screen';
 
 // Use Redux
 import { Provider } from 'react-redux';
@@ -11,6 +13,11 @@ import { NavigationContainer } from '@react-navigation/native';
 import Routes from './routes';
 
 export default class App extends Component {
+  componentDidMount() {
+    // do stuff while splash screen is shown
+    // After having done stuff (such as async tasks) hide the splash screen
+    SplashScreen.hide();
+  }
   render() {
     return (
       <Provider store={store}>
